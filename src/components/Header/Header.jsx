5f36ea207dc6d5b619f6c6c8fee1d04bd1b44f3f
Akash Singh <akashsingh.dev1@gmail.com>
2025-12-12 15:16:36 +0530

    added Header component and its styling in the project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/investment-calculator-logo.png" alt="logo"></img>
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+};
+export default Header;
